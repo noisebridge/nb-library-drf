63d59f4b7a8514a75cc39ad4d392d2faf10ecd8a
munnu <moniqueblake4@gmail.com>
2017-08-15 21:32:28 -0700

    Added Book model and Location model.

--- library/api/models.py
+++ library/api/models.py
@@ -16,7 +16,28 @@ class Book(models.Model):
     '''
     
     # do we need to say a max length?
-    openlibrary_olid = models.CharField(max_length=255)
+    id = models.IntegerField(primary_key=True)
+    isbn = models.CharField(max_length=20)
+    olid = models.CharField(max_length=20)
+    lccn = models.CharField(max_length=20)
+    title = models.CharField(max_length=200)
+    authors = models.CharField(max_length=200)
+    publish_date = models.CharField(max_length=30)
+    number_of_pages = models.CharField(max_length=10)
+    subjects = models.CharField(max_length=5000)
+    openlibrary_medcover_url = models.CharField(max_length=500)
+    openlibrary_preview_url = models.CharField(max_length=500)
     openlibrary_description = models.TextField()
+    dewey_decimal_class = models.CharField(max_length=50)
+    location = models.ForeignKey('Location', on_delete=models.SET_NULL, null=True, default=None)
 
 
+
+class Location(models.Model):
+    ''' Locations have a shortname and a pkey ID. 
+    Books are linked to location by ForeignKey using the ID (pkey).
+    '''
+    id = models.IntegerField(primary_key=True)
+    label_name = models.CharField(max_length=20, unique=True)
+    full_name = models.CharField(max_length=100)
+    books = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True, blank=True)
